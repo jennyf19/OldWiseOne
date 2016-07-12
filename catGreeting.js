37812966097b9d30a71656be9bb065282ba54b59
Jennyf19 <ferriesj@gmail.com>
2016-07-12 11:52:09 -0700

    v1.0

---
+++ catGreeting.js
@@ -0,0 +1,18 @@
+<html>
+<style>
+  body {
+  background-color:blue;
+}
+</style> 
+<body>
+<h1> "Hello crazy cat people.
+This is a safe place for cats and their guardians." </h1>
+<h2> {
+font-family: Sans-serif;
+font-size: 30 px;
+color: white;
+}
+</h2>
+<p>"This website is dedicated to Stormy and Brio" </p>
+</body>
+</html>
